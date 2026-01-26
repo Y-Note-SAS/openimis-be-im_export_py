ba644b498b9befac131ad3ecea69e76ccd24c66c
Le-positif-clinton <105806913+Le-positif-clinton@users.noreply.github.com>
2026-01-26 09:48:43 +0100

    Fix: expiry date compute wit grace period

--- im_export/services.py
+++ im_export/services.py
@@ -873,10 +873,6 @@ class BankImportService:
                 policy = new_policy
 
             if policy:
-                from policy.values import set_expiry_date
-                set_expiry_date(policy)
-                policy.expiry_date += grace_period
-                policy.save()
                 premium_data = {
                     "audit_user_id": self._user.id,
                     "receipt": data.code_receipt,
