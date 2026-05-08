e01d40141334f10f7ef7dbf6c21d0bab4db72120
kevel choumene <k.choumene@cloudfacile.com>
2026-05-08 02:20:24 +0100

    feat: ajustement

--- im_export/services.py
+++ im_export/services.py
@@ -876,7 +876,7 @@ class BankImportService:
             message=f"Paiement reçu pour l'assuré {chf_id}, montant {amount_received} KMF pour la date du {payment_date.strftime('%d/%m/%Y')}"
         )
         
-        premium = self.create_premium(chf_id, payment_invoice)
+        premium = self.create_premium(chf_id, payment_invoice, code_tp)
         return {
             "invoice_code": invoice.code,
             "payment_id": payment_invoice.id,
@@ -886,7 +886,7 @@ class BankImportService:
             "amount": str(invoice.amount_total),
         }
 
-    def create_premium(self, chf_id, data):
+    def create_premium(self, chf_id, data, code_tp):
         try:
             insuree = Insuree.objects.get(chf_id=chf_id, validity_to__isnull=True)
             family = Family.objects.get(head_insuree=insuree, validity_to__isnull=True)
@@ -941,7 +941,7 @@ class BankImportService:
                     "audit_user_id": self._user.id,
                     "receipt": data.code_receipt,
                     "pay_date": data.date_payment,
-                    "pay_type": "B",
+                    "pay_type": "B" if code_tp in ["BDC", "EXIM", "Banque"] else "M",
                     "is_photo_fee": False,
                     "amount": data.amount_received,
                     "policy": policy
