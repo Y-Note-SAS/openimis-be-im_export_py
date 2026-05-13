79dfa785da1e66dbf784ae03fde605f2db5950a0
kevel choumene <k.choumene@cloudfacile.com>
2026-05-13 14:23:32 +0100

    fix(Expiry date): fix expiry date on payment ref36655

--- im_export/services.py
+++ im_export/services.py
@@ -31,6 +31,7 @@ import os
 from pathlib import Path
 import copy
 from policy.services import update_insuree_policies
+from policy.services import policy_status_premium_paid
 
 logger = logging.getLogger(__name__)
 
@@ -946,6 +947,12 @@ class BankImportService:
                 premium = Premium(**premium_data)
                 created = update_or_create_premium(premium, self._user)
                 logger.info(f"Contribution créée avec succès pour police {policy.id}")
+                policy_status_premium_paid(
+                    policy,
+                    premium.pay_date
+                    if premium.pay_date > policy.start_date
+                    else policy.start_date,
+                )
                 return created
             else:
                 logger.warning(f"Aucune police active trouvée pour la famille {family.id}")
