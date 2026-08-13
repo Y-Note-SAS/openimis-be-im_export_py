af49bdbfa7116d5480296c1c24d65a9004fd9c88
kevel choumene <k.choumene@cloudfacile.com>
2026-08-13 13:31:22 +0100

    feat: ajustement

--- im_export/urls.py
+++ im_export/urls.py
@@ -1,20 +1,10 @@
-from dateutil.relativedelta import relativedelta
-from datetime import date
-from datetime import timedelta
+from . import views
+from django.urls import path
 
-nb_periods, remainder = divmod(1000, 2500)
-effective_date = date(2025, 6, 5)
-print(f"there is the expiry date before {effective_date}")
-# Calcul de la date d'expiration en fonction de la périodicité
-
-base_expiry= effective_date + relativedelta(months=1)
-
-grace_days = 2 * 30
-grace_period = timedelta(days=grace_days) if grace_days else timedelta(0)
-expiry_date = base_expiry + grace_period
-print(f"there is the grace period {grace_period}")
-print(f"there is the expiry date after {expiry_date}")
-a = 0
-if not a:
-    print("Ok..")
-
+urlpatterns = [
+    path("exports/insurees", views.export_insurees),
+    path("imports/insurees", views.import_insurees),
+    path("imports/exim_bank", views.import_exim_bank),
+    path("imports/bdc_bank", views.import_bdc_bank),
+    path("imports/other_payment", views.import_other_payment_method),
+]
