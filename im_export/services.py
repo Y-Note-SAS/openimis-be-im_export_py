1de16a1846ccba20d00fe544980de76c1980dcbe
kevel choumene <k.choumene@cloudfacile.com>
2026-09-04 11:16:13 +0100

    feat: add date_valid_to on filter

--- im_export/services.py
+++ im_export/services.py
@@ -1129,7 +1129,8 @@ class BankImportService:
                 subject_id=family.head_insuree.id,
                 date_valid_from__date__gte=date_due,
                 is_deleted=False,
-                thirdparty_type=73
+                thirdparty_type=73,
+                date_valid_to__isnul=True
             ).exclude(status=Invoice.Status.CANCELLED).order_by("date_valid_from")
         logger.info("existing invoices:: %s ", existing_invoices)
         if existing_invoices:
